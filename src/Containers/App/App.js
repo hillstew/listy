675a92c99092bb911099ab6ab197ada80f36b586
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-13 13:27:21 -0700

    Fetch notes in App and store in state

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,51 +1,62 @@
-import React, { Component } from 'react';
-import { NavLink, Route } from 'react-router-dom';
-import NotesSection from '../NotesSection/NotesSection';
-import NoteForm from '../NoteForm/NoteForm';
-import NotFound from '../../Components/NotFound/NotFound';
-import { connect } from 'react-redux';
-import { setNotes, setLoading } from '../../actions';
+import React, { Component } from "react"
+import { NavLink, Route } from "react-router-dom"
+import NotesSection from "../NotesSection/NotesSection"
+import NoteForm from "../NoteForm/NoteForm"
+import NotFound from "../../Components/NotFound/NotFound"
+import { connect } from "react-redux"
+import { setNotes, setLoading, setError } from "../../actions"
 
 class App extends Component {
-
-  componentDidMount() {
-    const { setNotes, setLoading } = this.props;
-    const notes = [];
-    setNotes(notes);
-    setLoading(false);
+  async componentDidMount() {
+    const { setNotes, setLoading } = this.props
+    try {
+      const response = await fetch("http://localhost:3001/api/v1/notes")
+      const notes = await response.json()
+      setNotes(notes)
+      setLoading(false)
+    } catch (error) {
+      setError(error.message)
+    }
   }
 
   render() {
-    const { notes } = this.props;
+    const { notes } = this.props
     return (
       <div>
         <header>
-          <NavLink id='title' to='/'>
+          <NavLink id="title" to="/">
             <h1>Listy</h1>
           </NavLink>
-          <NavLink id='add-note-link' to='/new-note'>
-            <i className="fas fa-plus-circle"></i>
+          <NavLink id="add-note-link" to="/new-note">
+            <i className="fas fa-plus-circle" />
           </NavLink>
         </header>
-        <Route exact path='/' component={NotesSection} />
-        <Route exact path='/new-note' component={NoteForm} />
-        <Route exact path='/notes/:id' render={({ match }) => {
-          const id = match.params;
-          const note = notes.find(note => note.id == id)
-          if (note) {
-            return <NoteForm />
-          } else {
-            return <NotFound />
-          }
-        }} />
+        <Route exact path="/" component={NotesSection} />
+        <Route exact path="/new-note" component={NoteForm} />
+        <Route
+          exact
+          path="/notes/:id"
+          render={({ match }) => {
+            const id = match.params
+            const note = notes.find((note) => note.id == id)
+            if (note) {
+              return <NoteForm />
+            } else {
+              return <NotFound />
+            }
+          }}
+        />
       </div>
-    );
+    )
   }
 }
+
+
 
 const mapDispatchToProps = (dispatch) => ({
   setNotes: (notes) => dispatch(setNotes(notes)),
   setLoading: (loading) => dispatch(setLoading(loading)),
+  setError: (error) => dispatch(setError(error))
 })
 
 export default connect(null, mapDispatchToProps)(App);
