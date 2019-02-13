eb0121d69d7b88a5347479eddd0ddd9df6478287
Karin Ohman <kaohma@gmail.com>
2019-02-13 13:24:46 -0700

    Resolve merge conflict

---
+++ src/utils/api.js
@@ -0,0 +1,22 @@
+const getData = async (path) => {
+
+}
+
+const postData = async (path, data) => {
+
+}
+
+const putData = async (path, data) => {
+
+}
+
+const deleteData = async (path) => {
+
+}
+
+export default {
+  getData,
+  postData,
+  putData,
+  deleteData,
+}
