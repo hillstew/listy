05521ed8ac34aebb05778d7121f164d4223c4ea1
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-14 15:14:13 -0700

    Update Header to receive togglePopup as a prop

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-export const Header = () => {
+export const Header = ({ togglePopup }) => {
   return (
     <header>
       <NavLink id="title" to="/">
         <h1>Listy</h1>
       </NavLink>
-      <NavLink id="add-note-link" to="/new-note">
+      <NavLink
+        onClick={() => togglePopup(true)}
+        id="add-note-link"
+        to="/new-note">
         <i className="fas fa-plus-circle new-note-icon" />
       </NavLink>
     </header>
