fbbebc2c0d764e30c5e9cbb5931b15870ffb6d6d
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-14 14:36:19 -0700

    Connect Header to Redux store and add onClick to NavLink

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { togglePopup } from '../../thunks/togglePopup'
+import { withRouter } from 'react-router-dom'
+
+
 
 export const Header = () => {
   return (
@@ -7,9 +12,15 @@ export const Header = () => {
       <NavLink id="title" to="/">
         <h1>Listy</h1>
       </NavLink>
-      <NavLink id="add-note-link" to="/new-note">
+      <NavLink onClick={() => this.props.togglePopup(true)} id="add-note-link" to="/new-note">
         <i className="fas fa-plus-circle new-note-icon" />
       </NavLink>
     </header>
   )
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  togglePopup: () => dispatch(togglePopup())
+})
+
+export default withRouter(connect(null, mapDispatchToProps)(Header))
