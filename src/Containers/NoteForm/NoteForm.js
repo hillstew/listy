750cb63975aee6c86ca6883f5db97b4e7b4dda29
Karin Ohman <kaohma@gmail.com>
2019-02-13 19:02:34 -0700

    Add functionality to add/remove as many issues as you want

--- src/Containers/NoteForm/NoteForm.js
+++ src/Containers/NoteForm/NoteForm.js
@@ -49,6 +49,21 @@ class NoteForm extends Component {
     }
   }
 
+  addIssue = (e) => {
+    e.preventDefault();
+    const issues = this.state.issues.slice();
+    issues.push({ id: Date.now(), body: '', completed: false });
+    this.setState({ issues });
+  }
+
+  removeIssue = (e) => {
+    e.preventDefault();
+    const index = this.state.issues.findIndex((issue) => issue.id == e.target.parentElement.id);
+    const issues = this.state.issues.slice();
+    issues.splice(index, 1);
+    this.setState({ issues });
+  }
+
   showIssues = (completed) => {
     const { issues } = this.state;
     return issues.filter(issue => issue.completed === completed).map(issue =>
@@ -57,7 +72,7 @@ class NoteForm extends Component {
           <i onClick={this.toggleIssueCompletion} className="fas fa-square"></i>
           <input onChange={this.handleBodyChange} value={issue.body}></input>
         </span>
-        <i className="fas fa-times"></i>
+        <button onClick={this.removeIssue}>Delete</button>
       </li>
     )
   }
@@ -75,7 +90,7 @@ class NoteForm extends Component {
           <form className='note-pop-up' onSubmit={this.handleSubmit} ref={(el) => this.formRef = el}>
             <input onChange={this.handleTitleChange} value={title}></input>
             <ul>{incompleteIssues}</ul>
-            <i className="fas fa-plus-circle form-add-icon"></i>
+            <button onClick={this.addIssue}><i className="fas fa-plus-circle form-add-icon"></i></button>
             <h4>Completed</h4>
             <ul>{completeIssues}</ul>
             <input className='submit-button' type="submit" value='Save'></input>
