d253e5689ebc86ae1cfdfbe8a2325cf8d759b357
Heather Hartley <hlhartley@gmail.com>
2019-02-17 17:53:54 -0700

    Adds SASS to 404 page not found

--- src/Components/AlternateScreen/AlternateScreen.js
+++ src/Components/AlternateScreen/AlternateScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from "prop-types"
 
 const AlternateScreen = ({ text }) => {
-  return <h4 className='screen-text'>{text}</h4>
+  return <div className='container-404'><h4 className='screen-text'><i class="fas fa-exclamation-triangle"></i> <p className='text-404'>{text}</p></h4></div>
 }
 
 export default AlternateScreen;
