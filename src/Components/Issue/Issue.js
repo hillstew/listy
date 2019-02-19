1894d1e4c7b06fbc2d5a1b7d28748510c5ddbe5b
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-19 13:50:29 -0700

    Refactor Issue to conditional render input or not based on pathname

--- src/Components/Issue/Issue.js
+++ src/Components/Issue/Issue.js
@@ -1,32 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const Issue = (props) => {
-  const { issue: { id, body }, toggleIssueCompletion, handleBodyChange, removeIssue, completed } = props;
+  const {
+    issue: { id, body }, location: { pathname }, 
+    toggleIssueCompletion, handleBodyChange, removeIssue, completed, location, noteId } = props;
   return (
-    <li key={id} id={id} className='issue-li'>
-      <span>
-        <button 
+    <li key={id} id={id} className="issue-li">
+      <div>
+        <button
           onClick={toggleIssueCompletion}
           className={completed ? 'complete-item' : 'incomplete-item'}
         />
-        <input
-          onChange={handleBodyChange}
-          placeholder='Add a list item...'
-          value={body}
-          maxLength='45'
-        />
-      </span>
-      <button onClick={removeIssue} className='list-delete-button'/>
+        {(pathname === `/notes/${noteId}` || pathname === '/new-note') && (
+          <input
+            onChange={handleBodyChange}
+            placeholder="Add a list item..."
+            value={body}
+            maxLength="45"
+          />
+        )}
+        {pathname === '/' && <Fragment>{body}</Fragment>}
+      </div>
+      {pathname === `/notes/${noteId}` && (
+        <button onClick={removeIssue} className="list-delete-button" />
+      )}
     </li>
-  )
-}
+  );
+};
 
 export default Issue;
 
 Issue.propTypes = {
-  handleBodyChange: PropTypes.func.isRequired,
-  removeIssue: PropTypes.func.isRequired,
-  issue: PropTypes.object.isRequired,
-  toggleIssueCompletion: PropTypes.func.isRequired,
-}
+  handleBodyChange: PropTypes.func,
+  removeIssue: PropTypes.func,
+  issue: PropTypes.object,
+  toggleIssueCompletion: PropTypes.func
+};
