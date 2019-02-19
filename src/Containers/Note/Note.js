45cd2ee2aeeac38f5e64383391e7b345749d00f4
Karin Ohman <kaohma@gmail.com>
2019-02-19 10:51:24 -0700

    Refactor Note renderIssues

--- src/Containers/Note/Note.js
+++ src/Containers/Note/Note.js
@@ -7,6 +7,7 @@ import { getIndex, createIssuesCopy } from '../../Helpers/functions';
 
 export const Note = ({note, putNote}) => {
   const { id, title, issues } = note;
+
   const toggleIssueCompletion = (e) => {
     const index = getIndex(e.target.id, issues);
     const newIssues = createIssuesCopy(issues);
@@ -14,44 +15,29 @@ export const Note = ({note, putNote}) => {
     putNote({ id, title, issues: newIssues });
   };
 
-  const renderIncompleteIssues = (issues) => {
-    return issues.filter((issue) => !issue.completed)
+  const renderIssues = (completed) => {
+    const iconClass = completed ? 'fas fa-check-square': 'fas fa-square'; 
+    return issues.filter((issue) => issue.completed === completed)
       .map((issue) => (
         <li key={issue.id} id={issue.id}>
           <div className='issue-body'>
             <i
-              className='fas fa-square'
+              className={iconClass}
               onClick={toggleIssueCompletion}
               id={issue.id}
             />
             {issue.body}
           </div>
         </li>
-      ));
-  };
-
-
-  const renderCompleteIssues = (issues) => {
-    return issues.filter((issue) => issue.completed)
-      .map((issue) => (
-        <li key={issue.id} id={issue.id}>
-          <div className='issue-body'>
-            <i
-              className='fas fa-check-square'
-              onClick={toggleIssueCompletion}
-              id={issue.id}
-            />
-            {issue.body}
-          </div>
-        </li>
-      ));
+      )
+    )
   };
 
   return (
     <div className='note-card'>
       <h3>{title}</h3>
-      <ul className='incomplete-list'>{renderIncompleteIssues(issues)}</ul>
-      <ul className='completed-list'>{renderCompleteIssues(issues)}</ul>
+      <ul className='incomplete-list'>{renderIssues(false)}</ul>
+      <ul className='completed-list'>{renderIssues(true)}</ul>
       <Link to={`/notes/${note.id}`}>
         <button className='edit-button-note' />
       </Link>
